15f63daec313791874054140f357b1607b42f9d3
Yurii Soroka <yurisoroka165@gmail.com>
2023-02-07 22:16:28 +0200

    to work

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+
+
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+// Notiflix.Notify.warning('Memento te hominem esse');
+// Notiflix.Notify.info('Cogito ergo sum');
